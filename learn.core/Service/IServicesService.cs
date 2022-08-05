c35a313994b0c4078ec90c0f5ae5206d421f8d63
yazan99726 <yazantayem26@gmail.com>
2022-08-05 12:18:23 +0300

    Create Folder Data Crud Operation For ServicesTable

---
+++ learn.core/Service/IServicesService.cs
@@ -0,0 +1,13 @@
+﻿using learn.core.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace learn.core.Service
+{
+    public interface IServicesService
+    {
+        public T CrudServices<T>(Services services, string httpMethod);
+        public Services GetServiceById(int id);
+    }
+}
---
+++ learn.infra/Service/ServicesService.cs
@@ -0,0 +1,28 @@
+﻿using learn.core.Data;
+using learn.core.Repoisitory;
+using learn.core.Service;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace learn.infra.Service
+{
+    public class ServicesService : IServicesService
+    {
+        private readonly IServicesRepository servicesRepository;
+
+        public ServicesService(IServicesRepository servicesRepository)
+        {
+            this.servicesRepository = servicesRepository;
+        }
+        public T CrudServices<T>(Services services, string httpMethod)
+        {
+            return servicesRepository.CrudServices<T>(services, httpMethod);
+        }
+
+        public Services GetServiceById(int id)
+        {
+            return servicesRepository.GetServiceById(id);
+        }
+    }
+}
