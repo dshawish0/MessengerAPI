8b4edb0c1b8b4613a4eedb7d262890da027c2c69
DEIA'A ALSHAWISH <daalshawish20@cit.just.edu.jo>
2022-08-05 18:25:38 +0300

    Deiaa

---
+++ MessengerAPI/Controllers/UserController.cs
@@ -0,0 +1,39 @@
+﻿using Messenger.core.Data;
+using Messenger.core.DTO;
+using Messenger.core.Service;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MessengerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService userService;
+
+        public UserController(IUserService userService)
+        {
+            this.userService = userService;
+        }
+
+        [HttpPost]
+        public IActionResult InsertUser([FromBody] UserLogDTO userlog)
+        {
+            var result = userService.InsertUser(userlog);
+            Console.WriteLine(userlog.UserId);
+            Console.WriteLine(userlog.User_Id);
+
+            Login log = new Login();
+            log.Email = userlog.Email;
+            log.Password = userlog.Password;
+            log.User_Id = userlog.User_Id;
+
+            return Ok(result);
+        }
+    }
+}
