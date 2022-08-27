05fdff43ce69926e0c719b08d5eb15c5293b94d8
DEIA'A ALSHAWISH <daalshawish20@cit.just.edu.jo>
2022-08-27 16:19:50 +0300

    add home footer fix MG Deiaa

--- learn.infra/Repoisitory/MessageGroupRepoisitory.cs
+++ learn.infra/Repoisitory/MessageGroupRepoisitory.cs
@@ -37,21 +37,18 @@ namespace learn.infra.Repoisitory
 
         public string DeleteMessageGroup(int id)
         {
-            //var parameter = new DynamicParameters();
-            //parameter.Add("crud ", "D", dbType: DbType.String, direction: ParameterDirection.Input);
-            //parameter.Add("MMessageGroupId", id, dbType: DbType.Int32, direction: ParameterDirection.Input);
-            //var result = dBContext.dbConnection.ExecuteAsync("MessageGroupCRUD_Package.MessageGroupCRUD", parameter, commandType: CommandType.StoredProcedure);
-
-            //if (result == null)
-            //{
-            //    return "Notdelete";
-            //}
-            //else
-            //{
-            //    return "deleted";
-            //}
-
-            return null;
+            var parameter = new DynamicParameters();
+            parameter.Add("crud ", "D", dbType: DbType.String, direction: ParameterDirection.Input);
+            parameter.Add("MMessageGroupId", id, dbType: DbType.Int32, direction: ParameterDirection.Input);
+            var result = dBContext.dbConnection.Execute("MessageGroupCRUD_Package.MessageGroupCRUD", parameter, commandType: CommandType.StoredProcedure);
+            if (result == null)
+            {
+                return "Notdelete";
+            }
+            else
+            {
+                return "deleted";
+            }
         }
 
         public List<MessageGroup> GetAllMessageGroup()
